e8656466a97f384b5bcd7972d767b96f8c3fe8d3
Jeffrey Zitelli <jeffrey.zitelli@gmail.com>
2015-09-27 02:16:58 -0400

    make jshint happy

--- src/fx/text/controls/TextBox.js
+++ src/fx/text/controls/TextBox.js
@@ -961,15 +961,11 @@ Primrose.Text.Controls.TextBox = ( function ( ) {
     //
     // different browsers have different sets of keycodes for less-frequently
     // used keys like.
-    browser = isChrome 
-    ? "CHROMIUM" 
-    : ( isFirefox 
-        ? "FIREFOX" 
-        : ( isIE 
-            ? "IE"
-            : ( isOpera 
-                ? "OPERA" 
-                : ( isSafari ? "SAFARI" : "UNKNOWN" ) ) ) );
+    browser = isChrome ? "CHROMIUM" : ( 
+      isFirefox ? "FIREFOX" : (
+        isIE ? "IE" : (
+          isOpera ? "OPERA" : (
+            isSafari ? "SAFARI" : "UNKNOWN" ) ) ) );
 
     //
     // the `surrogate` textarea makes the soft-keyboard appear on mobile devices.
