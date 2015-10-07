29d45a3e7906d61e186bd9b445b5fb5c3856ce1f
Jeffrey Zitelli <zemedi@gmail.com>
2015-10-07 01:08:09 -0400

    gfdfgdfg

--- examples/WebVRStudio/leapmotion.js
+++ examples/WebVRStudio/leapmotion.js
@@ -1,24 +1,38 @@
 function addHands(scene) {
     var leapController = new Leap.Controller({frameEventName: 'animationFrame'});
     leapController.connect();
-    var palm0 = new THREE.Mesh(new THREE.SphereBufferGeometry(0.025));
     var leapRoot = new THREE.Object3D();
     leapRoot.position.y -= 0.2;
-    leapRoot.position.z -= 1.3;
+    leapRoot.position.z -= 0.666;
     scene.add(leapRoot);
-    leapRoot.add(palm0);
-    var palm1 = palm0.clone();
-    leapRoot.add(palm1);
-    var palms = [palm0, palm1];
+
+    var palms = [];
+    var palm = new THREE.Mesh(new THREE.SphereBufferGeometry(0.025));
+    palm.scale.set(1, 0.5, 1);
+    leapRoot.add(palm);
+    palms.push(palm);
+    var palm = palm.clone();
+    leapRoot.add(palm);
+    palms.push(palm);
+
     var tips = [[], []];
+    var joints = [[], []];
     for (var i = 0; i < 5; ++i) {
         var tip = new THREE.Mesh(new THREE.SphereBufferGeometry(0.005));
+        var joint = new THREE.Mesh(new THREE.SphereBufferGeometry(0.007));
         tips[0].push(tip);
         leapRoot.add(tip);
+        joints[0].push(joint);
+        leapRoot.add(joint);
+
         tip = tip.clone();
+        joint = joint.clone();
         tips[1].push(tip);
         leapRoot.add(tip);
+        joints[1].push(joint);
+        leapRoot.add(joint);
     }
+
     leapController.on('frame', onFrame);
     function onFrame(frame)
     {
@@ -26,6 +40,9 @@ function addHands(scene) {
             palms[i].position.set(hand.palmPosition[0] / 1000, hand.palmPosition[1] / 1000, hand.palmPosition[2] / 1000);
             hand.fingers.forEach(function (finger, j) {
                 tips[i][j].position.set(finger.tipPosition[0] / 1000, finger.tipPosition[1] / 1000, finger.tipPosition[2] / 1000);
+                joints[i][j].position.set(finger.bones[1].nextJoint[0] / 1000, finger.bones[1].nextJoint[1] / 1000, finger.bones[1].nextJoint[2] / 1000);
+                // finger.bones.forEach(function (bone, k) {
+                // });
             });
         });
     }
