71a3526e9d758e2533b0dd8f893bca92efa8ba0e
Jeffrey Zitelli <zemedi@gmail.com>
2015-11-08 12:20:04 -0500

    evc

--- pyserver/EVC.py
+++ pyserver/EVC.py
@@ -5,17 +5,26 @@ from networkx import DiGraph # conda install networkx
 from three import *
 
 
+# node attributes: dimensions, joint type, joint limits, recursive limit, neurons
+#   joint types: rigid, revolute, twist, universal, bend-twist, twist-bend, spherical
+# edge attributes: position, orientation, scale, reflection, terminal only
+
+
 def construct_phenotype(genotype):
-	phenotype = Object3D()
-	nodes = genotype.nodes(data=True)
-	n_visits = {node[0]: 0 for node in nodes}
-	root, rootData = nodes[0]
-	phenotype.add(rootData['mesh'])
-	return phenotype
+    phenotype = Object3D()
+    nodes = genotype.nodes(data=True)
+    n_visits = {node[0]: 0 for node in nodes}
+    root, rootData = nodes[0]
+    phenotype.add(rootData['mesh'])
+    return phenotype
+
+
+def cubesnake():
+    pass
 
 
 if __name__ == "__main__":
-	genotype = DiGraph()
-	genotype.add_node(0, mesh=Mesh(geometry=BoxGeometry(1,1,1), material=MeshBasicMaterial(color=0xff0000)))
-	phenotype = construct_phenotype(genotype)
-	print(phenotype.json())
+    genotype = DiGraph()
+    genotype.add_node(0, mesh=Mesh(geometry=BoxGeometry(1,1,1), material=MeshBasicMaterial(color=0xff0000)))
+    phenotype = construct_phenotype(genotype)
+    print(phenotype.json())
